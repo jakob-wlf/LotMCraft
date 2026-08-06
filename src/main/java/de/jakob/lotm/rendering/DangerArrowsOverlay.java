bf8143c4313941411aff2be9ce7aefe025eaeddd
ThzAbdou <saadsaad200666@gmail.com>
2026-08-06 18:52:28 +0100

    made the paper dagger model scale x0.5 smaller, just uploaded the textures for danger arrow overlay things

---
+++ src/main/java/de/jakob/lotm/rendering/DangerArrowsOverlay.java
@@ -0,0 +1,10 @@
+package de.jakob.lotm.rendering;
+
+import de.jakob.lotm.LOTMCraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(modid = LOTMCraft.MOD_ID, value = Dist.CLIENT)
+public class DangerArrowsOverlay {
+
+}
